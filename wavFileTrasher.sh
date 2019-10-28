5200011a1aa6b6d7ff9dacb8d866940d53d4c466
afouquet7 <40359971+afouquet7@users.noreply.github.com>
2019-10-28 10:14:41 +0100

    finds files and mv them

---
+++ wavFileTrasher.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#==========================================================================================
+# Script to find files and move them to a given path (in this case .Trash)		  =
+# The -exec {} is a find argument it returns the resault to {}. 			  =	
+# the + (plus sign) ensures that we runs as few mv operations as possible.		  =	
+#											  =
+# Script Name: wavFileTrasher.sh							  =
+#											  =
+# Author: Alex Fouquet									  =
+# Date : 28.10.2019									  =	
+#											  =	
+#											  =	
+# Run Information: run script with bin/bash from command line. 				  =	
+#											  =
+#											  =	
+# Error Log: Any errors or output associated with the script will be displayed in termianl=
+#==========================================================================================
+
+sudo find /Volumes/DgLdB_All_Mixes/ -name '.wav' -exec mv -t /Volumes/DgLdB_All_Mixes/.Trashes/ {} +
+
+echo "your files have been moved!"
+
